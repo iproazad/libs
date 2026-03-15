1c173e2118ac0e96308cbd6ae0301bde821fd26e
iproazad <125834441+iproazad@users.noreply.github.com>
2026-03-15 22:20:54 +0300

    Update vng.bat

--- vng.bat
+++ vng.bat
@@ -3,10 +3,6 @@
 adb.exe kill-server
 adb.exe start-server
 
-echo --- Phase 1: Pre-Injection Prep (Pulling Original Library) ---
-
-echo --- Phase 2: Deep Cleanup ---
-
 :: مسح سجلات التتبع والديمو (ShadowTracker)
 adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
 adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Demos
@@ -23,26 +19,12 @@ echo --- Phase 3: Injection & Permissions ---
 
 :: حذف الملفات القديمة لضمان نظافة العملية [cite: 15]
 
-adb shell rm -f /data/data/com.vng.pubgmobile/lib/libswappy.so
+adb shell rm -f /data/data/com.vng.pubgmobile/lib/libAkAudioVisiual.so
 
 :: رفع الملفات المعدلة وإعطاؤها صلاحيات كاملة [cite: 14, 15]
-adb push libgame.so /data/data/com.vng.pubgmobile/lib/libgame.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libgame.so
-
-adb push libswappy.so /data/data/com.vng.pubgmobile/lib/libswappy.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libswappy.so
-
-:: إنشاء مجلد التمويه ورفع مكتبة الهوك [cite: 14]
-adb shell mkdir -p /data/local/tmp/andriod
-adb push libkeysuperman.so /data/local/tmp/andriod/libkeysuperman.so
-adb shell chmod 777 /data/local/tmp/andriod/libkeysuperman.so
-
-echo --- Phase 4: Launch & Monitor ---
-
-:: إيقاف إجباري ثم تشغيل اللعبة 
-adb shell am force-stop com.vng.pubgmobile
+adb push libAkAudioVisiual.so /data/data/com.vng.pubgmobile/lib/libAkAudioVisiual.so
+adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libAkAudioVisiual.so
 
 
 echo Waiting for memory load...
-timeout /t 3
-
+timeout /t 1
