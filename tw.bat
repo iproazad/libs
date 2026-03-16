957ce9d157d3058f7ef66d793fd81f9b66aa30c5
iproazad <125834441+iproazad@users.noreply.github.com>
2026-03-16 15:47:07 +0300

    Update tw.bat

--- tw.bat
+++ tw.bat
@@ -24,7 +24,7 @@ echo --- Phase 3: Injection & Permissions ---
 
 :: حذف الملفات القديمة لضمان نظافة العملية [cite: 15]
 
-adb shell rm -f /data/data/com.rekoo.pubgm/lib/libswappy.so
+adb shell rm -f /data/local/tmp/andriod/libkeysuperman.so
 
 :: رفع الملفات المعدلة وإعطاؤها صلاحيات كاملة [cite: 14, 15]
 adb push libgame.so /data/data/com.rekoo.pubgm/lib/libgame.so
@@ -33,10 +33,6 @@ adb shell chmod 777 /data/data/com.rekoo.pubgm/lib/libgame.so
 adb push libswappy.so /data/data/com.rekoo.pubgm/lib/libswappy.so
 adb shell chmod 777 /data/data/com.rekoo.pubgm/lib/libswappy.so
 
-:: إنشاء مجلد التمويه ورفع مكتبة الهوك [cite: 14]
-adb shell mkdir -p /data/local/tmp/andriod
-adb push libkeysuperman.so /data/local/tmp/andriod/libkeysuperman.so
-adb shell chmod 777 /data/local/tmp/andriod/libkeysuperman.so
 
 echo --- Phase 4: Launch & Monitor ---
 
@@ -46,4 +42,3 @@ adb shell am force-stop com.rekoo.pubgm
 
 echo Waiting for memory load...
 timeout /t 3
-
